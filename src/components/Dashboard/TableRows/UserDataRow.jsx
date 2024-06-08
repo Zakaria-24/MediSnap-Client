e7dc5ecf973570eda9f5c136573326ddcefa9557
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-06-09 05:33:25 +0600

    user updated successfully with refresh ui

--- src/components/Dashboard/TableRows/UserDataRow.jsx
+++ src/components/Dashboard/TableRows/UserDataRow.jsx
@@ -1,52 +1,61 @@
 import PropTypes from 'prop-types'
-// import { useState } from 'react'
-// import UpdateUserModal from '../../Modal/UpdateUserModal'
-// import { useMutation } from '@tanstack/react-query'
-// import useAxiosSecure from '../../../hooks/useAxiosSecure'
-// import toast from 'react-hot-toast'
-// import useAuth from '../../../hooks/useAuth'
+import { useState } from 'react'
+import UpdateUserModal from '../../Modal/UpdateUserModal'
+import { useMutation } from '@tanstack/react-query'
+import useAxiosSecure from '../../../hooks/useAxiosSecure'
+import toast from 'react-hot-toast'
+import useAuth from '../../../hooks/useAuth'
 
-const UserDataRow = ({ user }) => {
-//   const { user: loggedInUser } = useAuth()
-//   const [isOpen, setIsOpen] = useState(false)
-//   const axiosSecure = useAxiosSecure()
-
-//   const { mutateAsync } = useMutation({
-//     mutationFn: async role => {
-//       const { data } = await axiosSecure.patch(
-//         `/users/update/${user?.email}`,
-//         role
-//       )
-//       return data
-//     },
-//     onSuccess: data => {
-//       refetch()
-//       console.log(data)
-//       toast.success('User role updated successfully!')
-//       setIsOpen(false)
-//     },
-//   })
+const UserDataRow = ({ user, refetch }) => {
+    const axiosSecure = useAxiosSecure()
+  const { user: loggedInUser } = useAuth()
+ // For Modal
+ const [isOpen, setIsOpen] = useState(false);
+ const openModal = () => {
+   setIsOpen(true);
+ };
+ const closeModal = () => {
+   setIsOpen(false);
+ };
 
 
-  //   modal handler
-//   const modalHandler = async selected => {
-//     if (loggedInUser.email === user.email) {
-//       toast.error('Action Not Allowed')
-//       return setIsOpen(false)
-//     }
+  const { mutateAsync } = useMutation({
+    mutationFn: async role => {
+      const { data } = await axiosSecure.patch(
+        `/user/${user?.email}`,
+        role
+      )
+      return data
+    },
+    onSuccess: data => {
+      refetch()
+      console.log(data)
+      toast.success('User role updated successfully!')
+      setIsOpen(false)
+    },
+  })
 
-//     const userRole = {
-//       role: selected,
-//       status: 'Verified',
-//     }
 
-//     try {
-//       await mutateAsync(userRole)
-//     } catch (err) {
-//       console.log(err)
-//       toast.error(err.message)
-//     }
-//   }
+    // modal handler
+  const modalHandler = async selected => {
+    // admin can not change his/her role when he loged in
+    if (loggedInUser.email === user.email) {
+      toast.error('Action Not Allowed')
+      return closeModal();
+    }
+
+    const userRole = {
+      role: selected,
+    //   status: 'Verified',
+    }
+
+    try {
+      await mutateAsync(userRole)
+    } catch (err) {
+      console.log(err)
+      toast.error(err.message)
+    }
+  }
   return (
     <tr>
         <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
@@ -84,7 +93,7 @@ const UserDataRow = ({ user }) => {
 
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <button
-        //   onClick={() => setIsOpen(true)}
+          onClick={openModal}
           className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'
         >
           <span
@@ -94,12 +103,13 @@ const UserDataRow = ({ user }) => {
           <span className='relative'>Update Role</span>
         </button>
         {/* Update User Modal */}
-        {/* <UpdateUserModal
+        <UpdateUserModal
           isOpen={isOpen}
-          setIsOpen={setIsOpen}
+          closeModal={closeModal}
+        //   setIsOpen={setIsOpen}
           modalHandler={modalHandler}
           user={user}
-        /> */}
+        />
       </td>
     </tr>
   )
