1c3d1a40ec8dccd2774d582a87cf4d6585a86d6c
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-06-11 21:20:06 +0600

    complete shop cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,33 +1,45 @@
 import CartDataRows from "../../components/Dashboard/TableRows/CartDataRows";
-import { useQuery } from '@tanstack/react-query';
-import useAxiosSecure from '../../hooks/useAxiosSecure'
-import useAuth from './../../hooks/useAuth';
-
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAuth from "./../../hooks/useAuth";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { IoBagCheckOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const {user} = useAuth()
-  const axiosSecure = useAxiosSecure()
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
 
-const {data: cartData = [] , refetch } = useQuery({
-  queryKey: ['CartData'],
-  queryFn: async () => {
-const {data} = await axiosSecure(`/selectedCarts/${user?.email}`);
-return data;
+  const { data: cartData = [], refetch } = useQuery({
+    queryKey: ["CartData"],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/selectedCarts/${user?.email}`);
+      return data;
 
-    // const response = await fetch('http://localhost:8000/api/v1/medicines/cart');
-    // const data = await response.json();
-    // return data.data;
-  },
-})
-console.log(cartData)
+      // const response = await fetch('http://localhost:8000/api/v1/medicines/cart');
+      // const data = await response.json();
+      // return data.data;
+    },
+  });
+  console.log(cartData);
 
-    return (
-        <>
-        <h1></h1>
-            {/* show specific seller medicines info  */}
+  return (
+    <>
+      <h1></h1>
+      {/* show specific seller medicines info  */}
       <div className="container mx-auto px-4 sm:px-8">
         <div className="py-8">
-        <h1 className='flex justify-center text-2xl font-bold underline'>YOUR SELECTED MEDICINES INFO.{" "}<span className="bg-green-600 px-2 py-1 rounded-xl text-white"><h1>{cartData?.length}</h1></span></h1>
+          <h1 className="flex justify-center text-2xl font-bold underline">
+            YOUR SELECTED MEDICINES INFO.{" "}
+            <div className="flex items-center">
+            <span className="bg-green-600 px-2 py-1 rounded-xl text-white">
+              <h1>{cartData?.length}</h1>
+            </span>
+
+            <span className="ml-2 text-3xl"><IoIosArrowRoundForward /></span>
+            <span  className="ml-2 text-2xl"><Link to="/checkout" className="text-3xl btn bg-green-600"><IoBagCheckOutline /></Link></span>
+            </div>
+          </h1>
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
               <table className="min-w-full leading-normal">
@@ -91,9 +103,13 @@ console.log(cartData)
                 </thead>
                 <tbody>
                   {/* medicines row data */}
-                  
+
                   {cartData.map((cart) => (
-                    <CartDataRows key={cart._id} cart={cart} refetch={refetch} />
+                    <CartDataRows
+                      key={cart._id}
+                      cart={cart}
+                      refetch={refetch}
+                    />
                   ))}
                 </tbody>
               </table>
@@ -101,8 +117,8 @@ console.log(cartData)
           </div>
         </div>
       </div>
-        </>
-    );
+    </>
+  );
 };
 
 export default Cart;
