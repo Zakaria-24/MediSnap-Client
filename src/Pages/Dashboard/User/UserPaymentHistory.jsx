9e0c52b9d6b49bb4d96c9f41ffdcc0fc9edc6b88
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-06-12 06:17:32 +0600

    complete user dashboard page

--- src/Pages/Dashboard/User/UserPaymentHistory.jsx
+++ src/Pages/Dashboard/User/UserPaymentHistory.jsx
@@ -1,10 +1,68 @@
-
+import { useQuery } from '@tanstack/react-query';
+import PaymentHistoryDataRows from '../../../components/Dashboard/TableRows/PaymentHistoryDataRows';
+import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const UserPaymentHistory = () => {
+const {user} = useAuth()
+const axiosSecure = useAxiosSecure()
+
+// get all payment history by specific use by email
+const {data: UserPaymentHistory = [] } = useQuery({
+    queryKey: ['paymentHistory', user?.email ],
+    queryFn: async () => {
+        const {data} = await axiosSecure(`/paymentHistory/${user?.email}`)
+        return data
+    }
+})
+
+console.log(UserPaymentHistory)
+
+
     return (
-        <div>
-            <h1>userPaymentHistory</h1>
+        <>
+        {/* show payment info  */}
+        <div className="container mx-auto px-4 sm:px-8 ">
+          <div className="py-8">
+            <h1 className="flex justify-center text-2xl font-bold underline">
+              Payment History
+            </h1>
+            <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+              <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table className="min-w-full leading-normal">
+                  <thead>
+                    <tr className="bg-green-100">
+                      
+                      <th
+                        scope="col"
+                        className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
+                      >
+                        TransactionID
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-semibold"
+                      >
+                        Payment Status
+                      </th>
+                      
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* advertisement row data */}
+                    {UserPaymentHistory.map((payment) => (
+                      <PaymentHistoryDataRows
+                        key={payment._id}
+                        payment={payment}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
+      </>
     );
 };
 
---
+++ src/components/Dashboard/TableRows/PaymentHistoryDataRows.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+
+const PaymentHistoryDataRows = ({payment}) => {
+    console.log(payment)
+    return (
+        <>
+        <tr>
+          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+            <p className="text-gray-900 whitespace-no-wrap">
+              {payment?.transactionId}
+            </p>
+          </td>
+          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+            <p className="text-gray-900 whitespace-no-wrap">
+            {payment?.status === "success" ? 
+            <>
+            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-600">
+              {payment?.status}
+            </span>
+            </>:<>
+            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-600">
+              {payment?.status}
+            </span>
+            </>}
+            </p>
+          </td>
+        </tr>
+      </>
+    );
+};
+
+export default PaymentHistoryDataRows;
