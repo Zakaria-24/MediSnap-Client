3de9a22ee418ec9b4ceec5e8c7b007f375696f35
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-07-07 15:47:29 +0600

    update, padding related problem of  navbar and footer

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="navbar bg-green-50 px-24 pt-4 fixed z-10">
+      <div className="navbar bg-green-50 px-6 pt-2 fixed z-10">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
